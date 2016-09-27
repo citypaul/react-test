b582923aae1717241a132761ed0aca0cd0fe5ccc
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2016-09-27 11:41:24 +0100

    tutorial 2 done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,39 @@
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="CommentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render:function(){
+    return(
+      <div className="CommentForm">
+      Hello world! I am a CommentForm.
+      </div>
+    )
+  }
+});
+
+
+var CommentBox= React.createClass({
+  render:function(){
+    return(
+      <div className="CommentBox">
+      <h1>Comments</h1>
+      <CommentList />
+      <CommentForm />
+      </div>
+    )
+  }
+});
+
+
+
 ReactDOM.render(
-  <h1>Hello JSX Again!</h1>,
-  document.getElementById('example')
+  <CommentBox />,
+  document.getElementById('content')
 );
